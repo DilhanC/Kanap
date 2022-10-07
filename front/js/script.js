66af2f55a8af0add0d4f94e3f174f26a8ff29fe9
Dilhan Ceran <dilhan.ceran98@gmail.com>
2022-10-07 14:06:43 +0200

    all kanaps added on homepage

--- front/js/script.js
+++ front/js/script.js
@@ -1,32 +1,27 @@
 fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
-    .then((data) => addProducts(data))
+    .then((kanaps) => addProducts(kanaps))
 
-        // colors: ["Blue","White","Black"],
-        // _id: "107fb5b75607497b96722bda5b504926",
-        // name: "Kanap Sinopé",
-        // price: 1849,
-        // imageUrl: "http://localhost:3000/images/kanap01.jpeg",
-        // description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        // altTxt: "Photo d'un canapé bleu, deux places"
+function addProducts(kanaps) {
 
-function addProducts(data) {
-    const {_id, imageUrl, altTxt, name, description} = data[0]
-    
+    kanaps.forEach(kanaps => {
+
+    const {_id, imageUrl, altTxt, name, description} = kanaps
     const anchor = makeAnchor(_id)
     const article = document.createElement ("article")
     const image = makeImage(imageUrl, altTxt)
     const h3 = makeH3(name)
     const p = makeParagraph(description)
 
-    appendElementsToArticle(article, image, h3, p)
+    appendElementsToArticle(article, [image, h3, p])
     appendArticleToAnchor(anchor, article)
+    })
 }
 
-function appendElementsToArticle(article, image, h3, p){
-    article.appendChild(image)
-    article.appendChild(h3)
-    article.appendChild(p)
+function appendElementsToArticle(article, arrayofarticle){
+    arrayofarticle.forEach((item) => {
+        article.appendChild(item)
+    })
 }
 
 function makeAnchor(id) {
@@ -40,7 +35,6 @@ function appendArticleToAnchor(anchor, article) {
     if (items) {
         items.appendChild(anchor)
         anchor.appendChild(article)
-        console.log(items)
     }
 }
 
